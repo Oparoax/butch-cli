fadb775095bad087db57e54f733b3aa964ef70ca
Joseph Dawes <JDawes820@gmail.com>
2024-04-15 23:46:37 +0100

    Created setup.py

---
+++ butch_OS/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='butch_OS',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='jdawes',
+    author_email='',
+    description='Command line app for ttrpg \'cities without numbers\''
+)
