808ee8ac67f6e87697b716121b81f4e0e0abbff8
Joe Dawes <22896609+Oparoax@users.noreply.github.com>
2024-10-07 17:55:08 +0100

    Setup files added and updated toml file

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='butch_OS',
     version='1.0',
+    install_requires=['progress', 'pyttsx3'],
     packages=['progress', 'pyttsx3'],
     url='',
     license='',
